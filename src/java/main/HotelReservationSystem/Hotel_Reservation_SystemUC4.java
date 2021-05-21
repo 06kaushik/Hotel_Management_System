ce333365f0c7dfaa6361b6daca610c8367043a08
06kaushik <himanshu06kaushik@gmail.com>
2021-05-21 13:28:25 +0530

    To find cheapest hotel

---
+++ src/java/main/HotelReservationSystem/Hotel_Reservation_SystemUC4.java
@@ -0,0 +1,78 @@
+package com.hotelreservation.system;
+
+import java.util.Scanner;
+
+public class HotelReservation_System {
+	
+	private Hotels a, b, c;
+	
+	public void setHotelDetails() {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the Date with Customer type");
+    	        String date=sc.next();
+    	
+		a= new Hotels("LeMaredian");
+		a.setRegularWeekDay(110);
+		a.setRegularWeekEnd(90);
+		
+		b = new Hotels("MayurItc");
+		b.setRegularWeekDay(160);
+		b.setRegularWeekEnd(60);
+		
+		c = new Hotels("BlueGrass");
+		c.setRegularWeekDay(220);
+		c.setRegularWeekEnd(150);
+
+		int index = date.indexOf(":");
+                String type = date.substring(0, index);
+		
+		int cost_a=0;
+		int cost_b=0;
+		int cost_c=0;
+		int day_index_start = 0;
+		int day_index_end = 0;
+		
+		while(day_index_start != -1) {
+			day_index_start = date.indexOf("(", day_index_start + 1);
+                        day_index_end = date.indexOf(")", day_index_end + 1);
+            
+                  if(day_index_start != -1) {
+            	     String day = date.substring(day_index_start + 1, day_index_end);
+            	     if (type.equalsIgnoreCase("regular")) {
+            	       if(day.equalsIgnoreCase("sun") || day.equalsIgnoreCase("sat")) {
+            		cost_a += a.getRegularWeekEnd();
+            		cost_b += b.getRegularWeekEnd();
+            		cost_c += c.getRegularWeekEnd();
+            	       }
+                       else {
+            	        cost_a += a.getRegularWeekDay();
+        		cost_b += b.getRegularWeekDay();
+        		cost_c += c.getRegularWeekDay();
+                       } 
+                     } 
+		  }
+		}
+		System.out.println(cost_a);
+		System.out.println(cost_b);
+		System.out.println(cost_c);
+		
+		String result = min(cost_a, cost_b, cost_c);
+		System.out.println(result);
+	}
+	
+	public  String min(int x, int y, int z) {
+		if(x<y && x<z) {
+			return a.getHotelName()+":"+ x;
+		}else if(y<x && y<z) {
+			return b.getHotelName()+":"+ y;
+		}else {
+			return c.getHotelName()+":"+ z;
+		}	
+	}
+	
+	public static void main(String[] args) {
+		HotelReservation_System system = new HotelReservation_System();
+		system.setHotelDetails();
+	}
+
+}
